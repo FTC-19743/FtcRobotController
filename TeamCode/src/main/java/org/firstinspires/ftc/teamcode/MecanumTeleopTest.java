ab8364493df97634fcb991fe07ddae5c31ca6f90
Robotics <1loverobots!>
2022-10-01 16:41:37 -0700

    driver oriented drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleopTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleopTest.java
@@ -18,6 +18,7 @@ public class MecanumTeleopTest extends LinearOpMode {
     DcMotor frontRightMotor;
     DcMotor backLeftMotor;
     DcMotor backRightMotor;
+
     public void runOpMode() {
         //teamUtil.init(this);
 
@@ -26,6 +27,19 @@ public class MecanumTeleopTest extends LinearOpMode {
         frontRightMotor = hardwareMap.get(DcMotor.class, "frm");
         backLeftMotor = hardwareMap.get(DcMotor.class, "blm");
         backRightMotor = hardwareMap.get(DcMotor.class, "brm");
+
+
+
+        // Retrieve the IMU from the hardware map
+        BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        // Technically this is the default, however specifying it is clearer
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        // Without this, data retrieving from the IMU throws an exception
+        imu.initialize(parameters);
+
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
 
         frontRightMotor.setPower(0);
         frontLeftMotor.setPower(0);
@@ -36,25 +50,86 @@ public class MecanumTeleopTest extends LinearOpMode {
         telemetry.addLine("Ready to start");
         telemetry.update();
 
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio, but only when
+        // at least one is out of the range [-1, 1]
+
+
         waitForStart();
         while (opModeIsActive()) {
-            double y = -gamepad1.right_stick_x;
-            double x = gamepad1.left_stick_x;
-            double rx = gamepad1.left_stick_y; // Remember, this is reversed!
+            /*
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x; //counteract imperfect strafing
+            double rx = gamepad1.right_stick_x; // Remember, this is reversed!
 
+            // Read inverse IMU heading, as the IMU heading is CW positive
+            //double botHeading = -imu.getAngularOrientation().firstAngle;
+            double botHeading = 1;
+
+            double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
+            double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
+
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+             */
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = gamepad1.left_stick_x; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            double botHeading = -imu.getAngularOrientation().firstAngle;
+
+            double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
+            double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            /*
+            //working code for robot centric drive
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y - x + rx);
+            double backLeftPower = (y + x + rx) ;
+            double frontRightPower = (y - x - rx) ;
+            double backRightPower = (y + x - rx) ;
+
+             */
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY - rotX + rx) / denominator;
+            double backLeftPower = (rotY + rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+
+            backRightMotor.setPower(backRightPower);
+
+            //code for different speeds
+            /*
             if(gamepad1.right_bumper == true){
-                frontLeftMotor.setPower(y + x + rx);
-                backLeftMotor.setPower(y - x + rx);
-                frontRightMotor.setPower(y - x - rx);
-                backRightMotor.setPower(y + x - rx);
+                frontLeftMotor.setPower(frontLeftPower);
+                backLeftMotor.setPower(backLeftPower);
+                frontRightMotor.setPower(frontRightPower);
+                backRightMotor.setPower(backRightPower);
             } else{
-                frontLeftMotor.setPower((y + x + rx)/2);
-                backLeftMotor.setPower((y - x + rx)/2);
-                frontRightMotor.setPower((y - x - rx)/2);
-                backRightMotor.setPower((y + x - rx)/2);
+                frontLeftMotor.setPower(frontLeftPower/2);
+                backLeftMotor.setPower(backLeftPower/2);
+                frontRightMotor.setPower(frontRightPower/2);
+                backRightMotor.setPower(backRightPower/2);
             }
+
+             */
 
         }
     }
 
 }
+
+
