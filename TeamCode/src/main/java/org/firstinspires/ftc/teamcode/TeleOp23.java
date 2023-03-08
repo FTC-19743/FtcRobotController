aed27691c431cc2a0dd0e92764f33d93294b58f4
Robotics <robotics>
2023-03-07 20:07:48 -0800

    turn coefficient change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp23.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp23.java
@@ -98,7 +98,7 @@ public class TeleOp23 extends LinearOpMode {
 
             double y = -gamepad1.left_stick_y; // Remember, this is reversed!
             double x = gamepad1.left_stick_x*1.1 ; // Counteract imperfect strafing
-            double rx = -gamepad1.right_stick_x;
+            double rx = (-gamepad1.right_stick_x)*0.5;
             if(Math.abs(rx)<0.15){
                 rx=0;
             }
